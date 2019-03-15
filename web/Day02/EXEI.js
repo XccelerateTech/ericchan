5c65288c8068ba42b3e776cc25a36dcc4be41b51
scatterrr <ericchanchungwa@gmail.com>
2019-03-15 16:25:07 +0800

    EXE I

---
+++ web/Day02/EXEI.js
@@ -0,0 +1,13 @@
+for(var i =1 ; i< 31 ; i++) {if (Number.isInteger(i/3) === true && Number.isInteger(i/5)  === false)
+    {console.log('Hong');}
+    else if (Number.isInteger(i/5) === true && Number.isInteger(i/3) ===false)
+    {console.log('Kong');}
+    else if (Number.isInteger(i/5) && Number.isInteger(i/3) === true)
+    {console.log('Hong Kong');}
+    
+    else {console.log(i);}
+} 
+
+
+
+
